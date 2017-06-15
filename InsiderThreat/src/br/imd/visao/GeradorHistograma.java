4629d12b391418fcd1c5c240b5c23de774913847
Clara Nobre <aclaranobre@gmail.com>
2017-06-15 19:03:41 -0300

    Novo Histograma criado

--- InsiderThreat/src/br/imd/visao/GeradorHistograma.java
+++ InsiderThreat/src/br/imd/visao/GeradorHistograma.java
@@ -1,81 +1,74 @@
 package br.imd.visao;
 
-import java.awt.Window;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
-
-import javax.swing.JPanel;
+import br.imd.controle.*;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.statistics.HistogramDataset;
-import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
-public class GeradorHistograma{
+/**
+ * 
+ * @author Ana Clara e Felipe Gilberto
+ * Classe GeradorHistograma irá gerar o Histograma dos dados de acesso dos usuários buscados
+ *
+ */
 
-	static String line = null;
-	static BufferedReader stream = null;
-	static ArrayList<String> csvData = new ArrayList<String>();
-	
-	public GeradorHistograma(String title) throws IOException {
-		JPanel chartPanel = crearPanel();
-		chartPanel.setPreferredSize(new java.awt.Dimension(500, 475));
-		setContentPane(chartPanel);
+public class GeradorHistograma extends ApplicationFrame{
+
+	public GeradorHistograma( String applicationTitle , String chartTitle ) {
+		super( applicationTitle );        
+		JFreeChart barChart = ChartFactory.createBarChart(
+				chartTitle,           
+				"Atividades",            
+				"Frequencia",            
+				createDataset(),          
+				PlotOrientation.VERTICAL,           
+				true, true, false);
+
+		ChartPanel chartPanel = new ChartPanel( barChart );        
+		chartPanel.setPreferredSize(new java.awt.Dimension( 560 , 367 ) );        
+		setContentPane( chartPanel ); 
 	}
 
-	private void setContentPane(JPanel chartPanel) {
-		// TODO Auto-generated method stub
-		
+	private CategoryDataset createDataset( ) {
+		final String logon_user = "Logon";        
+		final String device = "Device";        
+		final String acesso = "Sites";        
+		final String logon = "Logon";        
+		final String logoff = "Logoff";        
+		final String connect = "Connect";        
+		final String disconnect = "Disconnect";        
+		final DefaultCategoryDataset dataset = 
+				new DefaultCategoryDataset();  
+
+		dataset.addValue( 1.0 , logon_user , logon );        
+		dataset.addValue( 3.0 , logon_user , connect );        
+		dataset.addValue( 5.0 , logon_user , logoff ); 
+		dataset.addValue( 5.0 , logon_user , disconnect );           
+
+		dataset.addValue( 5.0 , device , logon );        
+		dataset.addValue( 6.0 , device , connect );       
+		dataset.addValue( 10.0 , device , logoff );        
+		dataset.addValue( 4.0 , device , disconnect );
+
+		dataset.addValue( 4.0 , acesso , logon );        
+		dataset.addValue( 2.0 , acesso , connect );        
+		dataset.addValue( 3.0 , acesso , logoff );        
+		dataset.addValue( 6.0 , acesso , disconnect );               
+
+		return dataset; 
 	}
 
-	private static IntervalXYDataset crearDataset() throws IOException {
-		HistogramDataset dataset = new HistogramDataset();
-		
-		try {
-			stream = new BufferedReader(new FileReader("Dados/device.csv"));
-			while ((line = stream.readLine()) != null) {
-				String[] splitted = line.split(",");
-				ArrayList<String> dataLine = new ArrayList<String>(splitted.length);
-				for (String data : splitted)
-					dataLine.add(data);
-				csvData.addAll(dataLine);
-			}
-		} finally {
-			if (stream != null)
-				stream.close();
-		}
-		return dataset;
-	}
-	private static JFreeChart crearChart(IntervalXYDataset dataset) {
-		JFreeChart chart = ChartFactory.createHistogram(
-				"Histograma", null, null, dataset, PlotOrientation.VERTICAL,
-				true, true,false);
-		
-		XYPlot plot = (XYPlot) chart.getPlot();
-		XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
-		renderer.setDrawBarOutline(false);
-		try{
-			ChartUtilities.saveChartAsJPEG(new File("histograma.jpg"), chart, 500, 475);
-		}
-		catch(IOException e){
-			System.out.println("Erro ao abrir o arquivo");
-		}
-		return chart;
-	}
-	
-	public static JPanel crearPanel() throws IOException {
-		JFreeChart chart = crearChart(crearDataset());
-		return new ChartPanel(chart);
+	public static void main(String[ ] args) {
+		GeradorHistograma chart = new GeradorHistograma("Histograma", 
+				"Atividades do Usuario");
+		chart.pack();        
+		RefineryUtilities.centerFrameOnScreen(chart);        
+		chart.setVisible(true); 
 	}
 }
